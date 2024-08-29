7438d254901744a4d81079533e8552fff1194509
MaksKarpov <ozuzuandvduvatel@gmail.com>
2024-08-29 23:10:11 +0300

    task 2 ready to js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,10 @@
+// Імпортуємо бібліотеку iziToast
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+    // iziToast.error({ // Показуємо повідомлення про помилку
+    //   message: 'Please choose a date in the future', // Текст повідомлення
+    //   position: 'topRight', // Позиція повідомлення: у верхньому правому куті
+    //   timeout: false // Час, протягом якого повідомлення буде відображатися
+    // });
